1d9c641bc5cee02724f6dc20c463d58a4f9d9b53
darrkenn <darrkenn@pm.me>
2026-02-16 19:48:30 +0000

    Improved fitness function performance, removed unnecessary collection and enumeration

--- src/chromosome.rs
+++ src/chromosome.rs
@@ -1,12 +1,16 @@
 use genetica::individual::{DynamicLengthIndividual, Generate, Individual, Mutate};
 use image::Rgb;
 use rayon::{
-    iter::{IntoParallelIterator, ParallelIterator},
+    iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator},
     slice::ParallelSlice,
 };
 
 use crate::{CONFIG, RGB, TARGET_IMAGE};
-use std::simd::{num::SimdUint, u8x4};
+use std::simd::{
+    f32x4,
+    num::{SimdFloat, SimdUint},
+    u8x4,
+};
 
 #[macro_export]
 macro_rules! generate_value {
@@ -74,32 +78,30 @@ impl Individual for Chromosome {
 
     fn calculate_fitness(&mut self) {
         let image = TARGET_IMAGE.get().unwrap();
+        let width = CONFIG.resize_dimensions.x;
 
-        let error_count: f32 = image
-            .data
-            .enumerate_pixels()
-            .collect::<Vec<_>>()
-            .par_chunks(CONFIG.resize_dimensions.total_pixels() as usize)
-            .map(|chunk| {
-                chunk
-                    .iter()
-                    .map(|(x, y, pixel)| {
-                        let i = (y * CONFIG.resize_dimensions.x as u32 + x) as usize;
-                        if i < self.genes.len() {
-                            let rgb = &self.genes[i].rgb.0;
+        let error_count: f32 = (0..CONFIG.resize_dimensions.total_pixels() as usize)
+            .into_par_iter()
+            .map(|i| {
+                if i < self.genes.len() {
+                    let x = (i % width as usize) as u32;
+                    let y = (i / width as usize) as u32;
+                    let rgb = &self.genes[i].rgb.0;
+                    let pixel = image.data.get_pixel(x, y);
+                    // 0 is just padding
+                    let pixel_vec = f32x4::from_array([
+                        pixel.0[0] as f32,
+                        pixel.0[1] as f32,
+                        pixel.0[2] as f32,
+                        0.0,
+                    ]);
+                    let rgb_vec =
+                        f32x4::from_array([rgb[0] as f32, rgb[1] as f32, rgb[2] as f32, 0.0]);
 
-                            // 0 is just padding
-                            let pixel_vec =
-                                u8x4::from_array([pixel.0[0], pixel.0[1], pixel.0[2], 0]);
-                            let rgb_vec = u8x4::from_array([rgb[0], rgb[1], rgb[2], 0]);
-
-                            let sum = (pixel_vec - rgb_vec).reduce_sum();
-                            sum as f32
-                        } else {
-                            0.0
-                        }
-                    })
-                    .sum::<f32>()
+                    (pixel_vec - rgb_vec).abs().reduce_sum()
+                } else {
+                    0.0
+                }
             })
             .sum();
 
